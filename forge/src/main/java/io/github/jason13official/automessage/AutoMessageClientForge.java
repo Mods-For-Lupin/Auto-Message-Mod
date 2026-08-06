92c3e07fd323d3ff5e257026afbb01cf1c25d9ef
jason13official <jason13official@gmail.com>
2026-08-06 13:11:06 -0400

    remove unused base

--- forge/src/main/java/io/github/jason13official/automessage/AutoMessageClientForge.java
+++ forge/src/main/java/io/github/jason13official/automessage/AutoMessageClientForge.java
@@ -1,13 +1,11 @@
 package io.github.jason13official.automessage;
 
-import java.util.function.Consumer;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 public class AutoMessageClientForge {
 
   public AutoMessageClientForge(final IEventBus modEventBus) {
 
-    modEventBus.addListener((Consumer<FMLClientSetupEvent>) event -> AutoMessageClient.init());
+    AutoMessageClient.init();
   }
 }
