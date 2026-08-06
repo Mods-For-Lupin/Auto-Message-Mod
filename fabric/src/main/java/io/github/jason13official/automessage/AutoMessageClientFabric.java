f88148a0db04d3bb2edd25d2ed1383d5191cb746
jason13official <jason13official@gmail.com>
2026-08-06 16:03:40 -0400

    impl missing client event

--- fabric/src/main/java/io/github/jason13official/automessage/AutoMessageClientFabric.java
+++ fabric/src/main/java/io/github/jason13official/automessage/AutoMessageClientFabric.java
@@ -1,7 +1,9 @@
 package io.github.jason13official.automessage;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 
 public class AutoMessageClientFabric implements ClientModInitializer {
 
@@ -9,5 +11,15 @@ public class AutoMessageClientFabric implements ClientModInitializer {
   public void onInitializeClient() {
 
     AutoMessageClient.init(Minecraft.getInstance());
+
+    ClientEntityEvents.ENTITY_LOAD.register((entity, clientLevel) -> {
+      if (!(entity instanceof LocalPlayer player) || clientLevel == null) return;
+
+      AutoMessageClient.init(Minecraft.getInstance());
+
+      AutoMessageClient.onFirstJoinLevel(player, clientLevel);
+      AutoMessageClient.onJoinLevel(player, clientLevel);
+    });
+  }
   }
 }
--- neoforge/src/main/java/io/github/jason13official/automessage/AutoMessageClientNeoForge.java
+++ neoforge/src/main/java/io/github/jason13official/automessage/AutoMessageClientNeoForge.java
@@ -1,12 +1,28 @@
 package io.github.jason13official.automessage;
 
+import java.util.function.Consumer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 
 public class AutoMessageClientNeoForge {
 
   public AutoMessageClientNeoForge(final IEventBus modEventBus) {
 
     AutoMessageClient.init(Minecraft.getInstance());
+
+    NeoForge.EVENT_BUS.addListener((Consumer<EntityJoinLevelEvent>) event -> {
+
+      Entity entity = event.getEntity();
+      Level level = event.getLevel();
+      if (!(entity instanceof LocalPlayer localPlayer) || !(level instanceof ClientLevel clientLevel)) return;
+      AutoMessageClient.onFirstJoinLevel(localPlayer, clientLevel);
+      AutoMessageClient.onJoinLevel(localPlayer, clientLevel);
+    });
   }
 }
